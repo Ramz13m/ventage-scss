b2c03caa37bf82fb021aa6217974813e8822728c
ramz <ramz.13m@mail.ru>
2022-05-08 01:19:52 +0500

    add id's for all sections

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   const list = document.querySelector(".header__nav-list");
   const modalButton = document.querySelector(".header__nav-burger-icon");
+  const heartIcon = document.querySelector(".header__nav-heart-icon-logo");
 
   modalButton.addEventListener("click", () => {
     list.classList.toggle("active");
   })
+
+  heartIcon.addEventListener("click", () => {
+    heartIcon.classList.toggle("header__icon-bg");
+  })
+
+
 
 })
