1ff1bdea7a8edf8c1e671ee591e491f0e9d2f5c8
ramz <ramz.13m@mail.ru>
2022-05-08 03:08:07 +0500

    change typed.js header title

---
+++ js/typed-script.js
@@ -0,0 +1,12 @@
+var typed = new Typed('.text-typed', {
+  strings: [
+    "Your vision, our expertise!",
+    "Your vision, our expertise!"
+  ],
+  stringsElement:null ,
+  showCursor : false,
+  cursorChar : '', 
+  typeSpeed: 120,
+  backSpeed: 120,
+  // loop: true
+});
