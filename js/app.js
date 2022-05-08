7645fd802f147581a19f4a5288cefab37d35afdf
ramz <ramz.13m@mail.ru>
2022-05-08 21:16:01 +0500

    change Animated Counter on Scroll and respondive

--- js/app.js
+++ js/app.js
@@ -11,7 +11,40 @@ document.addEventListener("DOMContentLoaded", () => {
     heartIcon.classList.toggle("header__icon-bg");
   });
 
-  // NUMBERS ANIMATION
+  // NUMBERS ANIMATION jquery
+
+  var a = 0;
+$(window).scroll(function() {
+
+  var oTop = $('.info-num__item').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $('.info-num__anime-num').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+
+          duration: 3000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    a = 1;
+  }
+
+});
 
   
 
